1672bd84e4a28566acd669b652144586278a4f44
Andrés Naves <andnama@gmail.com>
2022-09-13 13:47:57 +0200

    API layout for the R&M api

--- js/function.js
+++ js/function.js
@@ -4,6 +4,10 @@ let potatoes = "potatoes";
 
 
 if (value == 12) {
-    console.log("I have %d", value, potatoes)
+    console.log("I have %d", value, potatoes);
 }
+
+var card =
+    document.getElementById("cards").appendChild(card)
+
 console.log("Hello Wold!");
