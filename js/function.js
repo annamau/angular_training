84e10859db241572775d8fd3b004eb43810ca15e
Andrés Naves <andnama@gmail.com>
2022-09-15 12:17:28 +0200

    Created visual interface

--- js/function.js
+++ js/function.js
@@ -1,13 +1,1 @@
-let value = 12;
-
-let potatoes = "potatoes";
-
-
-if (value == 12) {
-    console.log("I have %d", value, potatoes);
-}
-
-var card =
-    document.getElementById("cards").appendChild(card)
-
-console.log("Hello Wold!");
+function putMe()
