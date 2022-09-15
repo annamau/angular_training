1b95f1f1582d94041f50c6ea9a119926f46b77c7
Andrés Naves <andnama@gmail.com>
2022-09-15 13:33:34 +0200

    Calculator created fully -- FINISHED

--- js/function.js
+++ js/function.js
@@ -1,1 +1,57 @@
-function putMe()
+let writeb = true
+let multiplyb = false
+let substractb = false
+let divideb = false
+let addb = false
+
+function putMe(num) {
+    var text = parseFloat(document.getElementById("text").textContent);
+    if (writeb) {
+        if (text == "0") {
+            document.getElementById("text").textContent = num;
+        }
+        else {
+            document.getElementById("text").textContent = text + "" + num;
+        }
+    } else if (multiplyb) {
+        document.getElementById("text").textContent = text * num;
+        multiplyb = false;
+        writeb = true;
+    } else if (divideb) {
+        document.getElementById("text").textContent = text / num;
+        divideb = false;
+        writeb = true;
+    } else if (substractb) {
+        document.getElementById("text").textContent = text - num;
+        substractb = false;
+        writeb = true;
+    } else if (addb) {
+        document.getElementById("text").textContent = text + num;
+        addb = false;
+        writeb = true;
+    }
+
+}
+
+function clearData() {
+    document.getElementById("text").textContent = 0;
+}
+
+function multiply() {
+    multiplyb = true;
+    writeb = false;
+}
+
+function substract() {
+    substractb = true;
+    writeb = false;
+}
+
+function add() {
+    addb = true;
+    writeb = false;
+}
+function divide() {
+    divideb = true;
+    writeb = false;
+}
